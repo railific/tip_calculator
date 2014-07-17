c7082610a4f3e524dce307caf67557171d6dbe6d
Marty <railific.google.com>
2014-07-17 16:55:14 +0700

    basic version with method

---
+++ tip_calculator3.rb
@@ -0,0 +1,36 @@
+## #! path to interpreter here...
+
+require 'bigdecimal'
+require 'bigdecimal/util'
+
+
+#tax = 12
+#tip = 10
+
+### End of user editable section ###
+
+puts "\nPlease enter the meal cost before tax"
+meal = gets.chomp.to_d
+puts "Please enter the tax percentage you wish to pay"
+tax = gets.chomp.to_d
+puts "Please enter the tip percentage you wish to pay"
+tip = gets.chomp.to_d
+
+
+def calc_percent(amount, percentage)
+  amount.to_d * percentage.to_d / 100
+end
+
+
+tax_value = calc_percent(meal, tax)
+meal_with_tax = meal.to_d + tax_value.to_d
+tip_value = calc_percent(meal, tip)
+total_cost = meal_with_tax + tip_value
+
+
+
+puts "\nThe pre-tax cost of your meal was $%0.02f." % meal
+puts "At %0.1f%%, tax for this meal is $%0.2f." % [tax, tax_value]
+puts "For a %0.1f%% tip, you should leave $%0.2f." % [tip, tip_value]
+puts "The grand total for this meal is then $%0.2f.\n\n" % total_cost
+
